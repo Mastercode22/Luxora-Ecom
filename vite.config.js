250a4b3eb23eec92ea524d0d5f75bcaa28e9b19b
Emmanuel Quarshie <emmanuelquarshie395@gmail.com>
2026-07-18 13:13:00 +0000

    Initial commit with Vite GitHub Actions setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LuxoraGift_ReactApp/',
+  base: '/LuxoraGift-ReactApp/',
   server: {
     host: '0.0.0.0',
   },
